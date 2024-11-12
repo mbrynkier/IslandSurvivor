212d80c8e29f9e1ad4f2cf250be4b7433b686cf6
Marcos Gabriel Brynkier <mbrynkier@gmail.com>
2024-11-12 21:51:27 +0100

    Create Base Character Parent

---
+++ ProjectIslandSurvivor/BaseCharacter.cpp
@@ -0,0 +1,44 @@
+#include "BaseCharacter.h"
+#include "raymath.h"
+
+BaseCharacter::BaseCharacter()
+{
+
+}
+
+void BaseCharacter::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle BaseCharacter::GetCollisionRec()
+{
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        width * scale,
+        height * scale
+    };
+}
+
+void BaseCharacter::tick(float deltaTime)
+{
+    worldPosLastFrame = worldPos;
+
+    // update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+
+        runningTime = 0.f;
+
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw the character
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.0, WHITE);
+}
